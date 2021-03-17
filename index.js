f8390c0eb60ca2b3e79824a734fbeb2eca53f558
Maxim Alayev <neurologicalstyle@gmail.com>
2021-03-16 19:55:37 -0700

    Changed port to work with Heroku

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 5000
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -53,6 +54,6 @@ app.post("/api/downvote-restaraunt", (req, res) => {
   res.send("Success")
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("running on port 5000")
 });
