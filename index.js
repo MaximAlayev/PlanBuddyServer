12cb82a015ea6577f176326eaf26943727e38ca6
Maxim Alayev <neurologicalstyle@gmail.com>
2021-03-16 20:10:15 -0700

    Allow Access Control Origin

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.options('*', cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 
 function orderedRestarauntList(pollId) {
